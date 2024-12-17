6b62561e076018daea75c7918a7b271d4274e2ce
dovelinnnnnn <823548797@qq.com>
2024-12-17 23:54:14 +0800

    init

---
+++ ONIMultiplayer/Game/UI/MainMenuPatch.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+namespace ONIMultiplayer.Game.UI
+{
+[HarmonyPatch]
+   
+    internal class MainMenuPatch
+    {
+        [HarmonyReversePatch]
+        [HarmonyPatch(typeof(MainMenu), "OnPrefabInit")]
+        public static void MainMenuOnPrefabInit_Transpiler(object __instance,ref int __result)
+        {
+
+            IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var list = Transpilers.Manipulator(instructions,
+                    item => item.opcode == OpCodes.Ldarg_1,
+                    item => item.opcode = OpCodes.Ldarg_0
+                ).ToList();
+                var mJoin = SymbolExtensions.GetMethodInfo(() => string.Join(null, null));
+                var idx = list.FindIndex(item => item.opcode == OpCodes.Call && item.operand as MethodInfo == mJoin);
+                list.RemoveRange(idx + 1, list.Count - (idx + 1));
+                return list.AsEnumerable();
+            }
+
+        }
+    }
+}
--- ONIMultiplayer/Patches.cs
+++ ONIMultiplayer/Patches.cs
@@ -1,22 +1,92 @@
 using HarmonyLib;
+using JetBrains.Annotations;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Linq;
 
 namespace ONIMultiplayer
 {
     public class Patches
     {
-        [HarmonyPatch(typeof(Db))]
-        [HarmonyPatch("Initialize")]
-        public class Db_Initialize_Patch
+        //[HarmonyPatch(typeof(Db))]
+        //[HarmonyPatch("Initialize")]
+        //public class Db_Initialize_Patch
+        //{
+        //    public static void Prefix()
+        //    {
+        //        Debug.Log("I execute before Db.Initialize!");
+        //    }
+
+        //    public static void Postfix()
+        //    {
+        //        Debug.Log("I execute after Db.Initialize!");
+        //    }
+        //}
+        
+    }
+    [HarmonyPatch(typeof(ProcGenGame.WorldGen), nameof(ProcGenGame.WorldGen.LoadSettings))]
+    // 例子的目的是为了移除上面那段代码
+    public static class ProcGenGameWorldGenLoadSettings_Patch
+    {
+        // 方法名是 Transpiler，表示这是一个 Transpiler，而不是一个 Patch
+        // 方法签名必须返回 IEnumerable<CodeInstruction> 这是IL指令集合
+        // 参数中必须包含 IEnumerable<CodeInstruction> instructions
+        // 还可以通过 ILGenerator generator 注入代码生成器，或通过 MethodBase method 注入原始方法的信息
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            public static void Prefix()
+            // 用于标记是否找到了 TooBigCaravanMassUsage
+            var foundMassUsageMethod = false;
+            // 用于记录需要移除的代码的起始和结束索引
+            var startIndex = -1;
+            var endIndex = -1;
+
+            // 通过传入的 IL 获取 ILcodes
+            var codes = new List<CodeInstruction>(instructions);
+            for (var i = 0; i < codes.Count; i++)
             {
-                Debug.Log("I execute before Db.Initialize!");
+                // 遍历 ILcodes，并查找 ret
+                if (codes[i].opcode == OpCodes.Newobj)
+                {
+                    // 如果找到了字符串，则 endIndex更改为当前位置
+                    if (foundMassUsageMethod)
+                    {
+                        endIndex = i;
+                        break;
+                    }
+                    else
+                    {
+                        // 如果没有找到，将startIndex设置为 ret 的下一个位置
+                        startIndex = i + 1;
+
+                        // 然后从 startIndex 开始继续查找，直到找到下一个 ret
+                        for (var j = startIndex; j < codes.Count; j++)
+                        {
+                            if (codes[j].opcode == OpCodes.Newobj) break;
+
+                            var strOperand = codes[j].operand as string;
+                            // 判定有没有要找的字符串
+                            if (strOperand == "DisplayClass98_0")
+                            {
+                                foundMassUsageMethod = true;
+                                break;
+                            }
+                        }
+                    }
+                }
             }
 
-            public static void Postfix()
+            // 移除代码
+            if (startIndex > -1 && endIndex > -1)
             {
-                Debug.Log("I execute after Db.Initialize!");
+                // 要删除代码不能直接删除，删除某行会让IL的编号混乱，其他跳转会混乱，因此用的是 Nop 空操作
+                codes[startIndex].opcode = OpCodes.Nop;
+                codes.RemoveRange(startIndex + 1, endIndex - startIndex - 1);
             }
+
+            // 返回修改后的指令集
+            return codes.AsEnumerable();
         }
     }
 }
